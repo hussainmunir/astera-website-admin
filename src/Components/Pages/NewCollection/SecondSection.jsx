4f0ef5a1110b8f0a46ad70648440d5f4c630ad8f
sardaruzair56 <116259618+sardaruzair56@users.noreply.github.com>
2024-04-06 03:58:05 +0500

    after adding collection page

--- src/Components/Pages/NewCollection/SecondSection.jsx
+++ src/Components/Pages/NewCollection/SecondSection.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 
-export function SecondSection(props) {
-  const [images, setImages] = useState([]);
-  
+export function SecondSection({ saveImage }) {
+  const [previousImages, setPreviousImages] = useState([]);
+  const [newImages, setNewImages] = useState([]);
+  const [uploadedImage, setUploadedImage] = useState(null);
 
   const handleImageUpload = (event) => {
     const files = event.target.files;
-    const updatedImages = [...images];
+    const updatedNewImages = [];
 
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
@@ -14,14 +15,13 @@ export function SecondSection(props) {
 
       reader.onload = () => {
         const imageDataUrl = reader.result;
-        updatedImages.push(imageDataUrl);
-        setImages(updatedImages);
+        updatedNewImages.push({ id: i, dataUrl: imageDataUrl });
+        setNewImages((prevImages) => [...prevImages, ...updatedNewImages]);
       };
 
       reader.readAsDataURL(file);
     }
   };
-
   return (
     <div>
       <div>
@@ -34,12 +34,10 @@ export function SecondSection(props) {
           <input
             className="mt-6 ml-[20rem] border-2 border-black-500 border-solid p-3 rounded-lg"
             placeholder='WHAT"S NEW'
-            type="text"
           ></input>
           <input
             className="mt-6 ml-[2rem] border-2 border-black-500 border-solid p-3 rounded-lg"
             placeholder="COLLECTION 2024"
-            type="text"
           ></input>
         </div>
         <div className="flex">
@@ -53,69 +51,72 @@ export function SecondSection(props) {
         <div className="ml-8 mt-1 text-ellipsis text-slate-600">
           This will be displayed on your Section 3
         </div>
-
-        <div className="flex flex-wrap mt-4 mx-auto">
-          {images.map((image, index) => (
-            <div
-              key={index}
-              className="w-[220px] h-[170px]  border-2 border-neutral rounded-lg ml-[24.5rem] -mt-[3.5rem] p-2 absolute"
-            >
-              <img
-                src={image}
-                className="max-w-full max-h-full object-contain"
-                alt={`Image ${index}`}
-              />
-            </div>
-          ))}
-        </div>
-        <div
-          className="w-[350px] h-[170px] border-2 border-gray-300 border-solid rounded-lg p-6 ml-[40rem] absolute -mt-[3.5rem]"
-          id="dropzone"
-        >
-          <input
-            type="file"
-            className="absolute inset-0 w-full h-full opacity-0 z-50"
-            onChange={handleImageUpload}
-            multiple
-          />
-
-          <div className="text-center">
-            <img
-              className="mx-auto h-12 w-12"
-              src="https://www.svgrepo.com/show/357902/image-upload.svg"
-              alt=""
+        <div className="w-[350px] h-[170px] border-2 border-gray-300 border-solid rounded-lg p-6 ml-[24rem]">
+            <input
+              type="file"
+              className="absolute inset-0 w-full h-full opacity-0 z-50"
+              onChange={handleImageUpload}
+              multiple
             />
+            <div className="text-center">
+              <img
+                className="mx-auto h-12 w-12"
+                src="https://www.svgrepo.com/show/357902/image-upload.svg"
+                alt=""
+              />
+              <h3 className="mt-2 text-sm font-medium text-gray-900">
+                <label htmlFor="file-upload" className="relative cursor-pointer">
+                  <span>Drag and drop</span>
+                  <span className="text-indigo-600"> or browse</span>
+                  <span>to upload</span>
+                  <input
+                    id="file-upload"
+                    name="file-upload"
+                    type="file"
+                    className="sr-only"
+                    onChange={handleImageUpload}
+                    multiple
+                  />
+                </label>
+              </h3>
+              <p className="mt-1 text-xs text-gray-500">
+                PNG, JPG, SVG or GIF (max.800 * 400px)
+              </p>
+            </div>
+          </div>
 
-            <h3 className="mt-2 text-sm font-medium text-gray-900">
-              <label htmlFor="file-upload" className="relative cursor-pointer">
-                <span>Drag and drop</span>
-                <span className="text-indigo-600"> or browse</span>
-                <span>to upload</span>
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  className="sr-only"
-                  onChange={handleImageUpload}
-                  multiple
+        <div className="ml-8 mt-4">
+          <p className="font-bold text-lg">New Images</p>
+          {/* Display newly uploaded images */}
+          <div className="flex flex-wrap mt-4">
+            {newImages.map((image) => (
+              <div
+                key={image.id}
+                className="w-[220px] h-[170px] border-2 border-neutral rounded-lg p-2 mr-4 mb-4"
+              >
+                <img
+                  src={image.dataUrl}
+                  className="w-full h-full object-contain"
+                  alt={`Image ${image.id}`}
                 />
-              </label>
-            </h3>
-            <p className="mt-1 text-xs text-gray-500">
-              PNG, JPG, SVG or GIF (max.800 * 400px)
-            </p>
+              </div>
+            ))}
           </div>
         </div>
-        <div>
-          <p className="ml-8 mt-[10rem] font-bold text-lg">Catalog Image</p>
-          <img
-            className="ml-[10rem] -mt-5 w-4 h-4 absolute"
-            src={"src/assets/questiionmark.png"}
-            alt=""
-          />
-        </div>
       </div>
+      {/* Display temporarily uploaded image */}
+      {uploadedImage && (
+        <div className="ml-8 mt-4">
+          <p className="font-bold text-lg">Temporary Uploaded Image</p>
+          <div className="w-[220px] h-[170px] border-2 border-neutral rounded-lg p-2 mt-4">
+            <img
+              src={uploadedImage}
+              alt="Uploaded Image"
+              className="w-full h-full object-contain"
+            />
+          </div>
+        </div>
+      )}
     </div>
-    
   );
 }
