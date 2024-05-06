f52cb6879719c90f61416cf09dcbd3f2d1144463
hussainmunir <hussainmunir8080@gmail.com>
2024-05-06 15:15:48 +0500

    after completing home page

--- src/Components/Pages/HomePage/HomePage.jsx
+++ src/Components/Pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import {Section1} from "./Section1";
 import {Section2} from "./Section2";
 import Section3 from "./Section3";
 import Section5 from "./Section5";
-import Section4 from "./Section4/Title1";
+import Section4 from "./Section4/Section4";
 import Section6 from "./Section6";
 import TitlePage from "./TitlePage";
 
